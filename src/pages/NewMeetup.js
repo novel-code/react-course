f104ed7323382d7fd369e270d52b4ad7cd353442
syed <novelcode0@gmail.com>
2021-08-08 13:45:27 +0530

    react Context for state management

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from "react-router";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-simple-app-96747-default-rtdb.firebaseio.com/meetups.json",
@@ -11,7 +14,9 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then(() => {
+      history.replace("/");
+    });
   }
 
   return (
